5441b4ee7358038d01d3f14a1ec47475eff7b006
NazishTaj <153384749+NazishTaj@users.noreply.github.com>
2026-02-18 14:42:01 +0530

    Refactor settings.py for static file management
    Refactor settings to use pathlib for BASE_DIR and add static file settings.

--- ummeed_clone/settings.py
+++ ummeed_clone/settings.py
@@ -1,6 +1,16 @@
-
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 SECRET_KEY = 'dummy-secret-key'
 DEBUG = True
